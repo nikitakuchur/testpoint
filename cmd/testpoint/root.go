46dca1589438559853ad01fa86caebad507e876d
Nikita Kuchur <nikitakuchur@gmail.com>
2024-07-22 00:39:51 +0200

    fixed the version flag

--- cmd/testpoint/root.go
+++ cmd/testpoint/root.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"github.com/spf13/cobra"
+	"runtime/debug"
 )
 
 var version = ""
+
+// getVersion returns the current version of the application,
+// if it hasn't been overridden through the VERSION variable.
+func getVersion() string {
+	if version != "" {
+		return version
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+	return info.Main.Version
+}
 
 func newRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "testpoint",
 		Short:   "Testpoint is a simple CLI tool for testing REST endpoints",
-		Version: version,
+		Version: getVersion(),
 	}
 
 	cmd.Root().CompletionOptions.DisableDefaultCmd = true
